c8d9aa2c9ff4de4b938543d5b8aad36583791e5b
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2025-02-17 17:16:04 +0100

    created function for user registration

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home';
 import Locations from './components/locations';
 import About from './components/about';
 import VenueDetailPage from './components/venuedetail';
+import Register from './components/register'; //
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/locations" element={<Locations />} />
           <Route path="/about" element={<About />} />
           <Route path="/venue/:id" element={<VenueDetailPage />} /> 
